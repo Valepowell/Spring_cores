196e876f91f8be4e87ac5215f1fd0d844a5b724b
Valepowell <valepowell@gmail.com>
2025-12-18 20:20:26 -0300

    Add libros_y_autores and peiculas_y_directores projects

---
+++ Desktop/Spring_Cores/peiculas_y_directores/src/main/java/com/valeskapowell/controladores/controladorpelicula.java
@@ -0,0 +1,75 @@
+package com.valeskapowell.controladores;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class controladorpelicula {
+
+    private static HashMap<String, String> listaPeliculas = new HashMap<>();
+
+    // Constructor para llenar la lista de películas
+    public controladorpelicula() {
+        listaPeliculas.put("Winnie the Pooh", "Don Hall");	
+        listaPeliculas.put("El zorro y el sabueso", "Ted Berman");
+        listaPeliculas.put("Tarzán", "Kevin Lima");		
+        listaPeliculas.put("Mulán", "Barry Cook");
+        listaPeliculas.put("Oliver", "Kevin Lima");	
+        listaPeliculas.put("Big Hero 6", "Don Hall");	
+    }
+
+ //  Obtener todas las películas
+    @GetMapping("/peliculas")
+	public String obtenerTodasLasPeliculas() {
+		String peliculas = "";
+		
+		for(String pelicula : listaPeliculas.keySet()) {
+			String director = listaPeliculas.get(pelicula);
+			peliculas += pelicula + " - " + director + "<br>"; 
+		}
+		
+		return peliculas;
+	}
+	
+
+    //  Obtener película por nombre
+	@GetMapping("/peliculas/{nombre}")
+	public String obtenerPeliculas(@PathVariable String nombre) {
+		for(String pelicula : listaPeliculas.keySet()) {
+			if(pelicula.equalsIgnoreCase(nombre)) {
+				String director = listaPeliculas.get(pelicula);
+				return "Pelicula: " + pelicula + ", Director: " + director;
+			}
+		}
+		
+		return "La película no se encuentra en nuestra lista.";
+	}
+	
+	//  Obtener películas por director
+	@GetMapping("/peliculas/director/{nombre}")
+	public String obtenerPeliculasPorDirector(@PathVariable String nombre) {
+		String resultado = "";
+		boolean encontrado = false;
+		
+		for(String pelicula : listaPeliculas.keySet()) {
+			String director = listaPeliculas.get(pelicula);
+			
+			if(director.equalsIgnoreCase(nombre)) {
+				resultado += pelicula + "<br>";
+				encontrado = true;
+			}
+		}
+		
+		if(!encontrado) {
+			return "No contamos con películas con ese director en nuestra lista.";
+		}
+		
+		return "Peliculas de " + nombre + "<br>" + resultado;
+		
+	}
+}
